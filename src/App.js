3a9a259dd33da5ff05139fc1af919010b790f7f1
RohitMisshra <71648542+RohitMisshra@users.noreply.github.com>
2020-10-02 23:03:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ class App extends Component {
       return (
           <div className='App'>
             <h1>TO-DO List</h1>
-            <form action="" onSubmit={this.putTask}>
+            <form action="#" onSubmit={this.putTask}>
               <input type="text" name="data" onChange={this.newTask}/>
               <button action="submit">Add</button>
             </form>
